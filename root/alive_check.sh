0bc993c9eba9ee0c3f3f78570f1d7859a7667488
Vugar Afandi <evugar@gmail.com>
2013-03-21 20:07:41 +0000

    Remove test code.

--- root/alive_check.sh
+++ root/alive_check.sh
@@ -7,7 +7,7 @@ ping_options="-n -q"
 /bin/ping -c $request_count -w $wait_for_response $ping_options $host_to_ping > /dev/null 2>&1
 status=$?
 if [ $status -ne 0 ]; then
-    /usr/bin/netcfg -r  wireless-wpa > /root/log.txt 2>&1
+    /usr/bin/netcfg -r  wireless-wpa
 else
     echo "Alive!"
 fi
